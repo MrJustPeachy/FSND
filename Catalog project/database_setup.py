93088ece4b3b7adb5eb980890ec759fe397d9eca
Dillon Pietsch <MrJustPeachy@users.noreply.github.com>
2018-03-09 07:36:34 -0700

    Update database_setup.py

--- Catalog project/database_setup.py
+++ Catalog project/database_setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
